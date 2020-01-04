76e0c29672c421b8f934f3a49df4f4dc182790d2
robotevan <robotevan24@gmail.com>
2020-01-04 03:23:02 -0500

    Optimized read_distance, added device_status method to check health and status, comments

--- Lidar.py
+++ Lidar.py
@@ -6,8 +6,8 @@ DEFAULT_ADDRESS = 0x62 # Address of the device
 ACQ_COMMAND = 0x00 # W: 0x00 Reset all registers to default
                    # W: 0x03 Measure distance (no correction)
                    # W: 0x04 Measure distance (Bias correction)
-STATUS = 0x01
-DISTANCE_OUTPUT = 0x8f # Distance measurement in cm (2 Bytes)
+STATUS = 0x01 # R: Status register of the device
+DISTANCE_OUTPUT = 0x8f # R: Distance measurement in cm (2 Bytes)
 
 """
 Interface for the Garmin Lidar-Lite v3
@@ -16,15 +16,48 @@ class Lidar:
     def __init__(self):
         self.bus = SMBus(1)
 
-    def measure_distance(self, bias_correction=True):
+    """ 
+    Take one measurement in cm
+    @:param: bias_correction boolean, determines if measurement uses bias correction or not
+    @:return int distance in cm
+    """
+    def read_distance(self, bias_correction=True):
         if bias_correction:
             # Write 0x04 to 0x00 for bias corrected measurement
             self.bus.write_byte_data(DEFAULT_ADDRESS, ACQ_COMMAND,0x04)
         else:
             # Write 0x03 to 0x00 for no bias correction
             self.bus.write_byte_data(DEFAULT_ADDRESS, ACQ_COMMAND, 0x03)
-        # Wait and read 16 bits from the distance register
-        time.sleep(0.02)
+
+        # Wait for device to receive distance reading
+        while self.device_busy():pass
+        # Read HIGH and LOW distance registers
         distance = self.bus.read_i2c_block_data(DEFAULT_ADDRESS, DISTANCE_OUTPUT, 2)
-        time.sleep(0.02)
-        print(distance[0] << 8 | distance[1])
+        return distance[0] << 8 | distance[1] # combine both bytes
+
+    """
+    Read the STATUS register
+    :return: list of ints
+        bit 6: Process error flag
+        bit 5: Health flag
+        bit 4: Secondary return flag
+        bit 3: Invalid signal flag
+        bit 2: Signal overflow flag
+        bit 1: Reference overflow flag
+        bit 0: Busy flag
+    """
+    def device_status(self):
+        # Read the STATUS register, bits 0-6 only
+        current_config = int(self.bus.read_byte_data(DEFAULT_ADDRESS, STATUS))
+        # Convert to binary, fill rest with 0's
+        current_config = bin(current_config)[2:].zfill(7)
+        return [int(bit) for bit in str(current_config)]
+
+    """
+    Check if the device is busy
+    @:return boolean, true = busy
+    """
+    def device_busy(self):
+        # STATUS register bit 0 represents busy flag, 0 for ready, 1 for busy
+        return self.device_status()[-1]
+
